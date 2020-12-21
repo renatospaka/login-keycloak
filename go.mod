60b2e8d2a4b8a9d0955ad6abe75b4c8567852a1a
Renato Spakauskas <renatospaka@gmail.com>
2020-12-21 16:17:27 -0300

    Adjusting folder structure

---
+++ go.mod
@@ -0,0 +1,10 @@
+module goclient
+
+go 1.15
+
+require (
+	github.com/coreos/go-oidc v2.2.1+incompatible
+	github.com/pquerna/cachecontrol v0.0.0-20201205024021-ac21108117ac // indirect
+	golang.org/x/oauth2 v0.0.0-20201208152858-08078c50e5b5 // indirect
+	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
+)
---
+++ goclient/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	oidc "github.com/coreos/go-oidc"
+	"golang.org/x/oauth2"
+)
+
+var (
+	clientId = "myclient"
+	clientSecret = "bf6ba301-165f-4e42-b668-ad203d47ea3f"
+)
+
+func main() {
+	ctx := context.Background()	
+
+	provider, err := oidc.NewProvider(ctx, "http://localhost:8080/auth/realms/myrealm")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := oauth2.Config{
+		ClientID: clientId,
+    ClientSecret: clientSecret,
+    Endpoint: provider.Endpoint(),
+    RedirectURL: "http://localhost:8081/auth/callback",
+    Scopes: []string{oidc.ScopeOpenID, "profile", "email", "roles"},
+	}
+
+	state := "123UmaSenhaQualquer!"
+
+	http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
+		http.Redirect(response, request, config.AuthCodeURL(state), http.StatusFound)
+	})
+
+	http.HandleFunc("/auth/callback", func(response http.ResponseWriter, request *http.Request) {
+		if request.URL.Query().Get("state") != state {
+			http.Error(response, "State is invalid.", http.StatusBadRequest)
+			return 
+		}
+
+		//exchange the token sent by Keycloak for a access token
+		token, err := config.Exchange(ctx, request.URL.Query().Get("code"))
+		if err != nil {
+			http.Error(response, "Fail to exchange token.", http.StatusInternalServerError)
+			return
+		}
+			
+		//transforma o toke para JSON
+		resp := struct {AccessToken *oauth2.Token}{
+			token,
+		}
+		
+		data, err := json.Marshal(resp)
+		if err != nil {
+			http.Error(response, err.Error(), http.StatusInternalServerError) 
+			return
+		}
+
+		response.Write(data)
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
